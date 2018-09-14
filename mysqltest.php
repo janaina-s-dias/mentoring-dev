7de9577a55d8c420431acef86ca65be9a417d66e
Janaina Dias <janainasdias95@gmail.com>
2018-09-14 12:05:08 -0300

    Primeiro Commit

---
+++ mysqltest.php
@@ -0,0 +1,26 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+<?php
+    $servername = "localhost";
+    $username = "id7126546_admin";
+    $password = "admin";
+    $database = "id7126546_mentoringdb";
+try {
+        $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Conectado!"; 
+    } 
+    
+    catch(PDOException $e) {    
+        echo "Falhou...: " . $e->getMessage();
+    }
+    
+    echo "<br><a href='index.php'>Voltar</a>";
+?>
+
+<!-- Criar validação acesso ao nosso banco localhost -->
