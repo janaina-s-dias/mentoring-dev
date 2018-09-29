52c2efdec0d31a24d73dc9915017acaf51b84be2
paulohgama <santosps1990@gmail.com>
2018-09-29 16:47:05 -0300

    Model e Controller de Create, Update e Delete de Segment

--- app/Http/Controllers/SegmentController.php
+++ app/Http/Controllers/SegmentController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Model\Segment;
 
 class SegmentController extends Controller
 {
+    private $segment;
+    public function __construct(Segment $segment) {
+        $this->segment = $segment;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -34,7 +40,12 @@ class SegmentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, $this->segment->Rules(), $this->segment->messages);
+        $attributes = [
+            'segment_descrition' => $request->get('segment_descrition')
+        ];
+        $retorno = $this->segment->Store($attributes);
+        redirect()->with('mensagem', $retorno);
     }
 
     /**
@@ -68,7 +79,12 @@ class SegmentController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, $this->segment->Rules('update'), $this->segment->messages);
+        $attributes = [
+            'segment_descrition' => $request->get('segment_descrition')
+        ];
+        $retorno = $this->segment->Update($attributes, $id);
+        redirect()->with('mensagem', $retorno);
     }
 
     /**
@@ -79,6 +95,7 @@ class SegmentController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $retorno = $this->segment->Destroy($id);
+        redirect()->with('mensagem', $retorno);
     }
 }
--- app/Model/SegmentModel.php
+++ app/Model/SegmentModel.php
@@ -3,8 +3,56 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\QueryException;
+use App\Model\Segment;
 
 class Segment extends Model
 {
-    //
+    protected $table = ['segments'];
+    protected $fillable = ['segment_descrition'];
+    private $rules = [];
+    public $message = [];
+    public function Store($attributes)
+    {
+        $segment = new Segment($attributes);        
+        try
+        {
+            $segment->save();
+            return "Salvo com sucesso";
+        } catch (QueryException $exc) {
+            return "Erro ao salvar";
+        }
+    }
+    public function Update($attributes, $id)
+    {
+        $segment = Segment::find($id);
+        $segment->segment_descrition = $attributes->segment_descrition;
+        try
+        {
+            $segment->update();
+            return "Atualizado com sucesso";
+        } catch (QueryException $exc) {
+            return "Erro ao atualizar";
+        }
+    }
+    public function Destroy($id)
+    {
+        $segment = Segment::find($id);
+        try {
+            $segment->delete();
+            return "Deletado com sucesso";
+        } catch (QueryException $exc) {
+            return "Erro ao deletar";
+
+        }
+    }
+    public function Rules($type = 'insert')
+    {
+        switch ($type) {
+            case 'insert':
+                return $this->rules;
+            case 'update':
+                return $this->rules;
+        }
+    }
 }
