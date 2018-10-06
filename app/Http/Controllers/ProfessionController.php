874e3b260f76e8cd3a4a39e9cca65bbf2553449c
paulohgama <santosps1990@gmail.com>
2018-10-06 14:52:59 -0300

    Metodos de DataTable Adapatados

--- app/Http/Controllers/ProfessionController.php
+++ app/Http/Controllers/ProfessionController.php
@@ -12,40 +12,29 @@ class ProfessionController extends Controller
         $this->profession = $profession;
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
         $this->validate($request, $this->profession->Rules(), $this->profession->messages);
-        $attributes = [
-            'profession_descrition' => $request->get('profession_descrition')
-        ];
-        $retorno = $this->profession->Store($attributes);
-        redirect()->with('mensagem', $retorno);
+        $profession = new \App\Profession([
+            'profession_name' => $request->profession_name
+        ]);
+        try {
+            $profession->save();
+            redirect('profession.index')->with('success', 'Profissão salva');
+        } 
+        catch (\Illuminate\Database\QueryException $ex) {
+            redirect('profession.create')->with('failure', 'Não foi possivel cadastrar a profissão', $request);
+        }
+        
     }
 
     /**
@@ -56,7 +45,8 @@ class ProfessionController extends Controller
      */
     public function show($id)
     {
-        //
+        $profession = \App\Profession::find($id)->first();
+        redirect('profession.index')->with('finded', $profession);
     }
 
     /**
@@ -67,7 +57,8 @@ class ProfessionController extends Controller
      */
     public function edit($id)
     {
-        //
+        $profession = \App\Profession::find($id)->first();
+        return view('profession.editar')->with('finded', $profession);
     }
 
     /**
@@ -80,11 +71,16 @@ class ProfessionController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request, $this->profession->Rules('update'), $this->profession->messages);
-        $attributes = [
-            'profession_descrition' => $request->get('profession_descrition')
-        ];
-        $retorno = $this->profession->Update($attributes, $id);
-        redirect()->with('mensagem', $retorno);
+        $profession = \App\Profession::find($id)->first();
+        $profession->profession_name = $request->profession_descrition;
+        try
+        {
+            $profession->update();
+            redirect('profession.index')->with('success', 'Profissão alterada');
+        } catch (\Illuminate\Database\QueryException$ex) {
+            redirect('profession.editar')->with('failure', 'ERRO! Profissão não alterada');
+        }
+        
     }
 
     /**
@@ -95,32 +91,77 @@ class ProfessionController extends Controller
      */
     public function destroy($id)
     {
-        $retorno = $this->profession->Destroy($id);
-        redirect()->with('mensagem', $retorno);
+        $profession = \App\Profession::find($id)->first();
+        try
+        {
+            $profession->delete();
+            redirect('profession.index')->with('success', 'Profissão deletada');
+        } catch (\Illuminate\Database\QueryException$ex) {
+            redirect('profession.editar')->with('failure', 'ERRO! Profissão não deletada');
+        }
     }
 
 
-    public function PegaDados() {
-        $pegadados = $this->profession_model->criar_datatable();
+    public function PegaDados(Request $request) {
+        $pegadados = $this->CriarDataTable($request);
         $dados = array();
         foreach ($pegadados as $row) {
             $sub_dados = array();
-            $sub_dados[] = $row->profession_id;
-            $sub_dados[] = $row->profession_descrition;
-            $sub_dados[] = $row->profession_active;
-            $sub_dados[] = "<a href='".base_url('profession/editar')."/".$row->profession_id."' role='button' class='btn btn-success'><span class='glyphicon glyphicon-edit'></span></a>";
-            $sub_dados[] = "<a href='".base_url('profession/excluir')."/".$row->profession_id."' role='button' class='btn btn-danger'><span class='glyphicon glyphicon-trash'></span></a>";
+            $sub_dados[] = $row->profession_name;
+            $sub_dados[] = "<a href='". route('profession/edit', $row->profession_id)."' role='button' class='btn btn-success'><span class='glyphicon glyphicon-edit'></span></a>";
+            $sub_dados[] = "<a href='". route('profession/delete', $row->profession_id)."' role='button' class='btn btn-danger'><span class='glyphicon glyphicon-trash'></span></a>";
             $dados[] = $sub_dados;
         }
         
         $output = array (
-            "draw"  => intval($_POST["draw"]),
-            "recordsTotal" => $this->categoria_model->getAllData(), 
-            "recordsFiltered" => $this->categoria_model->getFilteredData(),
+            "draw"  => intval($request->draw),
+            "recordsTotal" => $this->TodosRegistros(), 
+            "recordsFiltered" => $this->RegistrosFiltrados($request),
             "data" => $dados
         );
         echo json_encode($output);
     }
-
+    
+    public function CriarQuery(Request $request)
+    {
+        $this->profession = \App\Profession::select($this->columnsSelect);
+        if(isset($request->search->value))
+        {
+            $this->profession->where('profession_name', 'like' ,'%', $request->search->value);            
+        }
+        if(isset($request->order))
+        {
+            $this->profession->orderBy(array_get($this->order, $request->input('order.0.column')),
+                                $request->input('order.0.dir'));
+        }
+        else
+        {
+            $this->profession->orderBy('profession_id', 'desc');
+        }
+    }
+    
+    public function CriarDataTable(Request $request)
+    {
+        $this->CriarQuery($request);
+        if($request->length != -1)
+        {
+            $this->profession->take($request->lenght)->skip($request->start);
+        }
+        $query = $this->profession->get();
+        return $query;
+    }
+    
+    public function RegistrosFiltrados(Request $request)
+    {
+        $this->CriarQuery($request);
+        $query = $this->profession->count();
+        return $query;
+    }
+    
+    public function TodosRegistros()
+    {
+        $profession = \App\Profession::all();
+        return $profession->count();
+    }
 
 }
