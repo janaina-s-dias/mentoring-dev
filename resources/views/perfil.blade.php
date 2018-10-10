8c3cb7b245797bf6464b990c4f047ceadc445ff0
pdonnarumma <astronomysaturn1997@gmail.com>
2018-10-10 12:54:35 -0300

    inicial - perfil e cadastro de assunto...

---
+++ resources/views/perfil.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.dashboard')
+@section('page_heading','Perfil')
+@section('section')
+         
+         <section class="arus" style="height: 250px; padding: 50px 50px; background: #b3ffd9;">
+
+         	<img class="tumbnail" src="{{ asset("svg/403.svg") }}"  alt="" style="width: 150px; height: 150px;">
+
+         	<div>
+         	<ul>
+            <li>Assunto...</li>
+            <li>Assunto...</li>
+            <li>Assunto...</li>
+            </ul>
+            </div>
+
+         </section>
+  
+         <section class="arcus" style="height: 200px; padding: 55px 55px; background: #80d4ff;">
+             
+             <h3>Conexões</h3>
+
+              
+         </section>  
+            
+@stop
