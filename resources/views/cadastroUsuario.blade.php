08b893cfde356b8219ff461a7685982ca7fc5163
AndreFrango <andre.mota86@hotmail.com>
2018-10-16 22:25:14 -0300

    Mask celular

--- resources/views/cadastroUsuario.blade.php
+++ resources/views/cadastroUsuario.blade.php
@@ -118,7 +118,15 @@
                $("#rg").mask('99.999.999-9'); 
                $("#cpf").mask('999.999.999-99'); 
                $("#telefone").mask('(99) 9999-9999'); 
-               $("#celular").mask('(99) 99999-9999', options);
+               $("#celular").mask('(00) 0000-00009');
+               $("#celular").blur(function(event){
+                   if ($(this).val().length == 15){
+                       $("#celular").mask('(00) 00000-0009');
+                   } else {
+                       $("#celular").mask('(00) 0000-0009');
+                   }
+                    });
+               
             });
         </script>
 </body>
