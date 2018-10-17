c704db1ba16e7653cc2dc0b132d16af451d98b36
Janaina Dias <janainasdias95@gmail.com>
2018-10-17 16:47:37 -0300

    dataTable Contato

---
+++ resources/views/manterContatos.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.dashboardAdmin') 
+@section('page_heading','Contatos')
+
+@section('section')
+<!-- <div>
+    <div class="input-group">
+                <span class="input-group-btn">
+                <left>
+                    <button class="btn btn-success" type="button" data-toggle="modal" data-toggle="tooltip" title="Inserir nova profissão" data-target="#myModal">
+                            <span class="glyphicon glyphicon-plus"></span>
+                    </button> 
+                </left>
+                </span>
+    </div> 
+</div>
+
+
+
+<br/> -->
+
+<div class="row">
+	<div>
+		@section ('table_panel_title','Listagem Contatos')
+		@section ('table_panel_body')
+		    @include('widgets.tableContato', array('class'=>'table-condensed'))
+		@endsection
+		@include('widgets.panel', array('header'=>true, 'as'=>'table'))
+	</div>
+</div>
+
+@include('inc.feedback')
+  
+            
+@stop
+
+
+  
---
+++ resources/views/widgets/tableContato.blade.php
@@ -0,0 +1,57 @@
+<meta name="csrf-token" content="{{ csrf_token() }}" />
+<script type="text/javascript"> 
+    $(document).ready(function (){
+       var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+       $('#tabelaContato').DataTable({
+           "processing": true,
+           "serverSide": true,
+           "order": [],
+           "dom": '<"top">rt<"bottom"ip><"clear">',
+           "ajax": {
+               "url": "{{ route('pegaDadosContato') }}", //url Controller Contato - PegaDados
+               "type": "POST",
+               "data": {_token: CSRF_TOKEN}
+           },
+           "columnDefs": [
+                {
+                    "targets": [ 4, 5 ], //quais colunas não possuirão a ordenação - editar/excluir
+                    "orderable":false
+                }
+           ],
+           "language": {
+                "zeroRecords": "Nada encontrado - desculpe",
+                "info": "Mostrando página _PAGE_ de _PAGES_",
+                "infoEmpty": "Nenhum registro disponivel",
+                "infoFiltered": "(filtrado do total de _MAX_ registros)",
+                "paginate": {
+                    "first":      "Primeira",
+                    "last":       "Última",
+                    "next":       "Próxima",
+                    "previous":   "Anterior"
+                },
+                "search":         "Pesquisar:",
+                "loadingRecords": "Carregando...",
+                "processing":     "Processando..."
+        },
+            "lengthChange": false,
+            "pageLength": 15
+       }); 
+    });
+</script>
+
+<div>
+<table id="tabelaContato" class="table-responsive">
+    <thead>
+        <tr>
+            <th>Código</th> 
+            <th>Tipo</th>
+            <th>Descrição</th>
+            <th>Usuario</th>
+            <th>Alterar</th>
+            <th>Excluir</th> 
+        </tr>
+    </thead>
+    <tbody>
+    </tbody>
+</table>
+</div>
