4170f5a39784eec9b33bb2934d0bf27d07201669
paulohgama <santosps1990@gmail.com>
2018-10-23 17:03:22 -0300

    Alterar senha alinhado

--- resources/views/alterarSenha.blade.php
+++ resources/views/alterarSenha.blade.php
@@ -21,24 +21,33 @@
              //dd($user);
              ?>
              
-             <form method="POST" action="{{ url('alterandoSenha',$user->user_id) }}" >
+             <form class="form-horizontal" method="POST" action="{{ url('alterandoSenha',$user->user_id) }}" >
                  {{ csrf_field() }}
 
                 <div class="form-group{{ $errors->has('user_hash') ? ' has-error' : '' }}">
-                    <input class="form-control" name="user_hash" id="senha" type="password" placeholder="Senha Atual">
+                    <label class="control-label col-sm-4" for="lastsenha">Senha Antiga:</label>
+                    <div class="col-sm-8">
+                        <input class="form-control" name="user_hash" id="lastsenha" type="password" placeholder="Senha Atual">
+                    </div>
                 </div>
 
                 <div class="form-group{{ $errors->has('user_hash') ? ' has-error' : '' }}">
-                    <input class="form-control" name="new_user_hash" id="novasenha" type="password" placeholder="Nova Senha">
+                    <label class="control-label col-sm-4" for="novasenha">Nova Senha:</label>
+                    <div class="col-sm-8">
+                        <input class="form-control" name="new_user_hash" id="novasenha" type="password" placeholder="Nova Senha">
+                    </div>
                 </div>
 
                 <div class="form-group{{ $errors->has('user_hash') ? ' has-error' : '' }}">
-                    <input class="form-control" name="new_user_hash_confirmation" id="confisenha" type="password" placeholder="Confirmação de Senha">
-
+                    
+                    <label class="control-label col-sm-4" for="confisenha">Confimação:</label>
+                    <div class="col-sm-8">
+                        <input class="form-control" name="new_user_hash_confirmation" id="confisenha" type="password" placeholder="Confirmação de Senha">
+                    </div>
                 </div> 
 
                 <div class="form-group"> 
-                    <div class="col-sm-offset-2 col-sm-10">
+                    <div class="col-sm-offset-4 col-sm-8">
                     <button type="submit" class="btn btn-success">Alterar Senha</button>
                     </div>
                 </div>   
