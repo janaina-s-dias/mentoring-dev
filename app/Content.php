7d25a4f8605db2225ced3db4c3bcc38e97cfb47d
paulohgama <santosps1990@gmail.com>
2018-10-25 09:21:22 -0300

    .

--- app/Content.php
+++ app/Content.php
@@ -11,20 +11,33 @@ class Content extends Model
     protected $fillable = [
         'content_content', 'content_title', 'content_url', 'content_type', 'fk_content_knowledge'
     ];
-    protected $rules = ['content_content' => 'sometimes|required|min:10|max:65000',
-                        'content_title' => 'required|min:10|max:100',
-                        'content_url' => 'sometimes|bail|required|active_url',
-                        'content_type' => 'bail|required|max:20'
+    protected $rules;
+    public $messages = [
+        'content_content.required' => 'O Conteudo deve ser preenchido!',                        
+        'content_content.min' => 'O Cnteudo deve conter mais de 10 caracteres!',
+        'content_content.max' => 'Voce estrapolou todos os 65mil caracteres',
+        'content_title.required' => 'O título é obrigatório!',
+        'content_title.min' => 'O título deve conter mais de 10 caracteres!',
+        'content_title.max' => 'O título deve ter no maximo 100 caracteres!',
+        'content_url.required' => 'Url é obrigatória!',
+        'content_url.active_url' => 'O campo deve ser preenchido com uma url válida!',
+        'content_type.required' => 'O campo é obrigatório!',
+        'fk_content_knowledge.required' => 'Campo de assunto obrigatorio'
+    ];
+
+    public function Regras($tipo = 'conteudo')
+    {
+        switch ($tipo)
+        {
+            case 'conteudo':
+                $this->rules = ['content_content' => 'bail|required|min:10|max:65000',
+                                'content_title' => 'bail|required|min:10|max:100',
+                                'content_type' => 'bail|required|max:20',
+                                'fk_content_knowledge' => 'required'
         
-    ];    
-     protected $messages = ['content_content.required' => 'O Conteudo deve ser preenchido!',                        
-                        'content_content.min' => 'O Cnteudo deve conter mais de 10 caracteres!',
-                        'content_content.max' => 'Voce estrapolou todos os 65mil caracteres',
-                        'content_title.required' => 'O título é obrigatório!',
-                        'content_title.min' => 'O título deve conter mais de 10 caracteres!',
-                        'content_title.max' => 'O título deve ter no maximo 100 caracteres!',
-                        'content_url.required' => 'Url é obrigatória!',
-                        'content_url.active_url' => 'O campo deve ser preenchido com uma url válida!',
-                        'content_type.required' => 'O campo é obrigatório!'
-         ];    
+    ];  
+            break;
+            
+        }
+    }
 }
